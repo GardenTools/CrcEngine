f2814c44939e86dfa562d283861f746d84caaa82
GardenTools <gardensofdorwinion@gmail.com>
2021-04-06 01:46:51 +0100

    Fixing comments so the GPL isn't shown as the module desciption

--- src/crcengine/__main__.py
+++ src/crcengine/__main__.py
@@ -1,4 +1,22 @@
 """Command line invocation for crcengine to either generate code or run on a file"""
+
+# This file is part of CrcEngine, a python library for CRC calculation
+#
+# Copyright 2021 Garden Tools software
+#
+# crcengine is free software: you can redistribute it an d /or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# crcengine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with crcengine.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import sys
 
--- src/crcengine/algorithms.py
+++ src/crcengine/algorithms.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 """
-This file is part of CrcEngine, a python library for CRC calculation
-
-Copyright 2021 Garden Tools software
-
-crcengine is free software: you can redistribute it an d /or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-crcengine is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with crcengine.  If not, see <https://www.gnu.org/licenses/>.
+CRC Algorithm parameterisation
 """
+
+# This file is part of CrcEngine, a python library for CRC calculation
+#
+# Copyright 2021 Garden Tools software
+#
+# crcengine is free software: you can redistribute it an d /or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# crcengine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with crcengine.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import generator_stop
 
 # Some of these polynomials are used for many algorithms, so they are collected
