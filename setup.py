72714cfad6b61afe96d4be250e390574059fd50e
GardenTools <gardensofdorwinion@gmail.com>
2023-04-14 23:02:57 +0100

    Removing setuptools files

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script. Kept only to preserve editable install, everything else
-is in setup.cfg"""
-import setuptools
-setuptools.setup()
